e6998fbb42ac23a537319f77829398910e598410
Lasecki99 <marcin.lasecki99@gmail.com>
2019-06-25 14:44:06 +0200

    Updates

---
+++ precache-manifest.8eb4201413a57556fde36cbe839455c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a307a38cb4c1d5909a11d9d713b9c2",
+    "url": "/Chuck-Norris-App/index.html"
+  },
+  {
+    "revision": "86407f6304b8cf5ced1d",
+    "url": "/Chuck-Norris-App/static/css/main.d5d0c856.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Chuck-Norris-App/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "86407f6304b8cf5ced1d",
+    "url": "/Chuck-Norris-App/static/js/main.b223babc.chunk.js"
+  },
+  {
+    "revision": "c25fd0f8259de13d32d9",
+    "url": "/Chuck-Norris-App/static/js/runtime~main.05f5963b.js"
+  }
+]);
